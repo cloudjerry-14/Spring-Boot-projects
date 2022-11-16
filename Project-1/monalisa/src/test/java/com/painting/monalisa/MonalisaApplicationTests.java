b1c73f34412a37762368ad75f211103683984150
Ashutosh Parihar <ashutoshparihar0@gmail.com>
2022-11-17 03:35:59 +0530

    deleted wine shop project

---
+++ Project-1/monalisa/src/test/java/com/painting/monalisa/MonalisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.painting.monalisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonalisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
